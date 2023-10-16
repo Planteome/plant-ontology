c6b27ad24cdadb5ed38a02e5e9a1e91cf9df8df5
Justin Elser <elserj@science.oregonstate.edu>
2023-10-16 11:07:11 -0700

    Add script to create glossary.js file for /po_glossary page

---
+++ scripts-etc/po_term_table_to_js.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+if ($#ARGV != 1) {
+		print "usage: po_term_table_to_js.pl term_table_file output_file\n";
+		exit;
+}
+
+my $termfile = $ARGV[0];
+my $outfile = $ARGV[1];
+
+open(TERM, "$termfile");
+open(OUTFILE, ">$outfile");
+
+print OUTFILE "var dataSet = [\n";
+
+while(<TERM>) {
+		my $line = $_;
+		chomp $line;
+		if($line !~ /^PO/) {
+				next;
+		}
+		
+		my ($id, $name, $defn, $synonyms, $isaid, $isaname) = split("\t", $line);
+		print OUTFILE "   [ \"$name\", \"<a href=\\\"https://browser.planteome.org/amigo/term/$id\\\">$id</a>\", \"$defn\" ],\n";
+}
+print OUTFILE "];\n";
+close(OUTFILE);
